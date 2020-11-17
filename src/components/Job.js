4b3afee971ee2eb8a2f71e9a79d2898d489db4e9
cnorteye <cnortye@gmail.com>
2020-11-16 18:18:38 -0700

    frontend job board app, updated api link once backend app was deployed to heroku, commented out setupProxy.js

---
+++ src/components/Job.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Job = ({ job, removeJob, editJob, editing }) => (
+    <div className="job" key={job.id}>
+      <h4>{job.company}</h4>
+      <p>{job.position}</p>
+      <p>{job.description}</p>
+
+      {editing ? (null
+      ) : (
+          <button
+              onClick={() => {editJob(job)}}
+          >Edit</button>
+      )}
+
+      <button onClick={() => removeJob(job.id)}>Remove</button>
+    </div>
+);
+
+export default Job;
---
+++ src/components/JobsList.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NewJobForm from './NewJobForm';
+import Job from './Job';
+import EditJobForm from './EditJobForm';
+
+const JobsList = props => {
+
+    useEffect(() => {
+      axios.get('https://cors-anywhere.herokuapp.com/https://comfy-backend-restapi.herokuapp.com/api/v1/jobs.json')
+          .then(res => setJobs(res.data))
+        }, []);
+  
+    const [jobs, setJobs] = useState([]);
+
+const initialFormState = {
+    company:'',
+    position:'',
+    description:''};
+
+const addJob = job => {
+  const qs = require('qs');
+
+  axios.post('https://cors-anywhere.herokuapp.com/https://comfy-backend-restapi.herokuapp.com/api/v1/jobs.json', qs.stringify(
+      {
+        job:{
+          company: job.company,
+          position: job.position,
+          description: job.description}
+      }))
+      .then(res=>( console.log(res)))
+      .catch( error => console.log(error))
+  
+  setJobs([...jobs, job]);
+};
+
+const removeJob = id => {
+    axios.delete( 'https://cors-anywhere.herokuapp.com/https://comfy-backend-restapi.herokuapp.com/api/v1/jobs.json' + id )
+        .then(response => {
+          setJobs(jobs.filter(job => job.id !== id))
+        })
+        .catch(error => console.log(error))
+  };
+
+const [editing, setEditing] = useState(false);
+const [currentJob, setCurrentJob] = useState(initialFormState);
+
+const editJob = job => {
+    setEditing(true);
+    setCurrentJob({
+      id: job.id,
+      company: job.company,
+      position: job.position,
+      description: job.description
+    })
+  };
+  
+  const updateJob = (updatedJob) => {
+    setEditing(false);
+  
+    const qs = require('qs');
+    axios.patch('https://cors-anywhere.herokuapp.com/https://comfy-backend-restapi.herokuapp.com/api/v1/jobs.json' + updatedJob.id, qs.stringify(
+        {
+          job:{
+            company: updatedJob.company,
+            position: updatedJob.position,
+            description: updatedJob.description}
+        }))
+        .then(
+            res=>(
+                console.log(res.data)
+            ));
+  
+    setJobs(jobs.map(job => (job.id === updatedJob.id ? updatedJob : job)))
+  };
+
+  return (
+    <div>
+    <div className="jobs-list">
+      <div>
+        {editing ? (
+          <EditJobForm
+              setEditing={setEditing}
+              currentJob={currentJob}
+              updateJob={updateJob}
+          />
+        ) : (
+          <NewJobForm addJob={addJob} initialFormState={initialFormState}/>
+        )}
+      </div>
+      <br/>
+      <hr/>
+      {jobs.map((job, _) => (
+          <Job job={job} removeJob={removeJob} editJob={editJob} editing={editing} />
+        ))}
+    </div>
+  </div>
+  )
+};
+export default JobsList;
